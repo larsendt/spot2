503c9ba66ba56300a2f869ea70c8efabe52016ed
Dane Larsen <dane.t.larsen@gmail.com>
2014-06-10 00:27:50 -0600

    added ph sensor

---
+++ components/ph_sensor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import generic_sensor as gs
+import spot_common as sc
+
+def take_reading():
+    value = 7.0
+    units = "pH"
+    gs.insert_reading("ph_sensor", value, units)
+
+if __name__ == "__main__":
+    try:
+        take_reading()
+    except Exception as e:
+        sc.log_exception(e)
