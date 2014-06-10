bb8bde6b3e4d4ba5eb3bb9662008d790102e77c2
Dane Larsen <dane.t.larsen@gmail.com>
2014-06-10 00:35:24 -0600

    added battery module

---
+++ components/battery.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import spot_common as sc
+import time
+
+DB = "battery_status.sqlite"
+CREATE_SQL = "CREATE TABLE IF NOT EXISTS battery_status (percent REAL, charging INTEGER, time REAL)"
+INSERT_SQL = "INSERT INTO battery_status VALUES (?,?,?)"
+
+def take_reading():
+    sc.init_db(DB, CREATE_SQL)
+    percent = 65.0
+    charging = 1
+    sc.insert(DB, INSERT_SQL, (percent, charging, time.time()))
+
+if __name__ == "__main__":
+    try:
+        take_reading()
+    except Exception as e:
+        sc.log_exception(e)
